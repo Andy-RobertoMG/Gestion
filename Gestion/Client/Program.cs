3b5c2e0e83611be2898298c4e2f21c8518750f1a
Andy Roberto Mesta Gonzalez <andyroberto49@gmail.com>
2024-09-14 21:30:56 -0600

    Fixed decimals.

--- Gestion/Client/Program.cs
+++ Gestion/Client/Program.cs
@@ -1,12 +1,14 @@
 using Blazored.Toast;
 using Gestion.Client;
+using System.Globalization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("es-MX");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("es-MX");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredToast();
 await builder.Build().RunAsync();
